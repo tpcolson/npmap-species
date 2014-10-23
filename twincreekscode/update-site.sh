ddeadd7be1c33d8fafd6beb9d459f5ebc5e140ce
John Duggan <jduggan1@vols.utk.edu>
2014-10-23 15:55:40 -0400

    removed source dir option

--- twincreekscode/update-site.sh
+++ twincreekscode/update-site.sh
@@ -10,11 +10,10 @@ PUB_FILES=("twincreeks.css" "config_gen.js" "form_utils.js" "background.jpg")
 PRIV_FILES=("send_email.js")
 USER="jduggan1"
 TAR_HOST="seelab.eecs.utk.edu"
-SRC_DIR="/home/jduggan1/npmap-species/twincreekscode" # currently must be on localhost
 TAR_DIR="/export/home/seelab/huangj/seelabwww/twincreeks"
 
 # let's copy over the files to the site
-scp ${SRC_DIR}/${INDEX} "${PUB_FILES[@]}" "${PRIV_FILES[@]}" ${USER}@${TAR_HOST}:${TAR_DIR}
+scp ${INDEX} "${PUB_FILES[@]}" "${PRIV_FILES[@]}" ${USER}@${TAR_HOST}:${TAR_DIR}
 
 # change file permissions as appropriate (also rename index)
 ssh ${USER}@${TAR_HOST} << ENDSSH
