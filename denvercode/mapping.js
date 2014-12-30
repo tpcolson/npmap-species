e8a240c7f5b61465f3368c0910dd569ad6d38dd9
John Duggan <jduggan1@vols.utk.edu>
2014-12-29 21:55:27 -0500

    removed geocoder, added fuseSearch

--- denvercode/mapping.js
+++ denvercode/mapping.js
@@ -1,75 +1,90 @@
-var NPMap = {
-	div: 'main-map',
-	baseLayers: [
-		'nps-parkTilesImagery', /* first item is the default baseLayer */
-		'nps-lightStreets',
-		'nps-neutralTerrain',
-		'nps-parkTiles',
-		'nps-satelliteNight',
-		'bing-aerial',
-		'bing-aerialLabels',
-		'bing-roads',
-		'esri-gray',
-		'esri-imagery',
-		'esri-nationalGeographic',
-		'esri-oceans',
-		'esri-streets',
-		'esri-topographic',
-		'mapbox-light',
-		'mapbox-outdoors',
-		'mapbox-satellite',
-		'mapbox-satelliteLabels',
-		'mapbox-streets',
-		'mapbox-terrain',
-		'openstreetmap',
-		'stamen-toner',
-		'stamen-watercolor'
-	],
-	//TODO: overlays need to be able to be toggled on and off (nothing too scary)
-	overlays: [{
-		name: 'Habitat',
-		id: 'lonnieljyu.test',
-		type: 'mapbox'
-	}, {
-		name: 'Observations',
-		url: 'Abies_fraseri.geojson',
-		type: 'geojson',
-		popup: {
-			title: 'Observation',
-			description: 'Coordinates: {{coordinates}}'
-		},
-		styles: {
-			point: {
-				'marker-color': '#7a904f',
-				'marker-size': 'small'
+var subNavZ, headerZ, divHeader, divSubNav,
+	NPMap = {
+		div: 'main-map',
+		baseLayers: [
+			'nps-parkTilesImagery', /* first item is the default baseLayer */
+			'nps-lightStreets',
+			'nps-neutralTerrain',
+			'nps-parkTiles',
+			'nps-satelliteNight',
+			'bing-aerial',
+			'bing-aerialLabels',
+			'bing-roads',
+			'esri-gray',
+			'esri-imagery',
+			'esri-nationalGeographic',
+			'esri-oceans',
+			'esri-streets',
+			'esri-topographic',
+			'mapbox-light',
+			'mapbox-outdoors',
+			'mapbox-satellite',
+			'mapbox-satelliteLabels',
+			'mapbox-streets',
+			'mapbox-terrain',
+			'openstreetmap',
+			'stamen-toner',
+			'stamen-watercolor'
+		],
+		//TODO: overlays need to be able to be toggled on and off (nothing too scary)
+		overlays: [{
+			name: 'Habitat',
+			id: 'lonnieljyu.test',
+			type: 'mapbox'
+		}, {
+			name: 'Observations',
+			url: 'Abies_fraseri.geojson',
+			type: 'geojson',
+			popup: {
+				title: 'Observation',
+				description: 'Coordinates: {{coordinates}}'
+			},
+			styles: {
+				point: {
+					'marker-color': '#7a904f',
+					'marker-size': 'small'
+				}
+			},
+			cluster: {
+				clusterIcon: '#7a904f'
+			},
+			showCoverageOnHover: true,
+			disableClusteringAtZoom: 15,
+			polygonOptions: {
+				color: '#7a904f',
+				fillColor: '#7a904f'
 			}
+		}],
+		zoom: 10,
+		center: { lat: 35.6, lng: -83.52 },
+		minZoom: 10,
+		maxZoom: 16,
+		maxBounds: [
+			{ lat: 35.25, lng: -84.25 },
+			{ lat: 35.9, lng: -82.75 }
+		],
+		//downloadControl: true, //TODO: this would be useful (once implemented, we could link to the relevant data store page)
+		fullscreenControl: true,
+		//shareControl: true, //TODO: this would be useful once implemented
+		printControl: true, //TODO: this won't work right until the geojsons are being pulled from non-local url
+		legendControl: {
+			html: setLegend
 		},
-		cluster: {
-			clusterIcon: '#7a904f'
-		},
-		showCoverageOnHover: true,
-		disableClusteringAtZoom: 15,
-		polygonOptions: {
-			color: '#7a904f',
-			fillColor: '#7a904f'
-		}
-	}],
-	zoom: 10,
-	center: { lat: 35.6, lng: -83.52 },
-	minZoom: 10,
-	maxZoom: 16,
-	maxBounds: [
-		{ lat: 35.25, lng: -84.25 },
-		{ lat: 35.9, lng: -82.75 }
-	],
-	fullscreenControl: true,
-	shareControl: true,
-	printControl: true,
-	legendControl: {
-		html: setLegend
-	},
-	//locateControl: true, //TODO: Check if Tom wants this
-	measureControl: true,
-	editControl: true,
-	scaleControl: true
-};
+		//locateControl: true, //TODO: Check if Tom wants this
+		measureControl: true,
+		editControl: true,
+		scaleControl: true,
+		events: [{
+			fn: function() {
+				setDivs();
+				enterfullscreen();
+			},
+			type: 'enterfullscreen'
+		}, {
+			fn: function() {
+				setDivs();
+				exitfullscreen();
+			},
+			type: 'exitfullscreen'
+		}]
+	};
