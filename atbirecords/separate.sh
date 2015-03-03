97f6462c8cca57783744ec6a049b6c43fa539531
Lonnie Yu <lyu6@vols.utk.edu>
2015-03-03 17:58:43 -0500

    Updated separate.sh

--- atbirecords/separate.sh
+++ atbirecords/separate.sh
@@ -4,5 +4,5 @@ rm -fr ATBI_files Geojsons
 . /usr/share/modules/init/bash
 module load python/2.7
 export PYTHONPATH=$HOME/local/lib/python2.7/site-packages:$PYTHONPATH
-python separate.py ATBI_records.csv
+python separate.py ATBI_records.csv > output.log
 
