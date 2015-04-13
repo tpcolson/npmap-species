657b9b12565180d1c242278b3c4bf43635703542
Lonnie Yu <lyu6@vols.utk.edu>
2015-04-13 18:18:28 -0400

    Creates filelist for species SSI computation.

---
+++ nautiluscode/maxent/create_filelist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if test $# -ne 1; then
+   echo 'usage: create_filelist.sh maxent_results_directory'
+   exit 1
+fi
+
+results_dir=$1
+
+ls -1 $results_dir | 
+while read line; do
+   sd_mean=$(cut -d' ' -f 2 $results_dir/$line/sd_stats.txt)
+   echo $results_dir/$line/$line\_avg.dat $sd_mean
+done > filelist
