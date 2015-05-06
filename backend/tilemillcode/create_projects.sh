0095861c7f00b4119a4f7cfad4753976334a4492
Long J Yu <lyu6@sonoran.eecs.utk.edu>
2015-05-06 19:57:39 -0400

    Modified Mapbox scripts.

--- backend/tilemillcode/create_projects.sh
+++ backend/tilemillcode/create_projects.sh
@@ -5,16 +5,16 @@ if test $# -ne 2; then
    exit 1
 fi
 
+dataset_prefix="GRSM"
+name_template=$dataset_prefix\_template
+geotiff_template=Abies_fraseri_colored.tif
+
 mapbox_dir=${1%/}
 project_dir=$mapbox_dir/project
 template_dir=$project_dir/template
 geotiff_symlink=$template_dir/templatecolored.tif
 #geotiff_path=$(readlink $geotiff_symlink | sed -r "s|/[^/]+$|/$sp|")
 geotiff_dir=${2%/}
-
-# echo "project_dir="$project_dir
-# echo "template_dir="$template_dir
-# echo "geotiff_dir="$geotiff_dir
 
 while read line; do
    for sp in $line; do
@@ -24,17 +24,13 @@ while read line; do
       layers_dir=$species_dir/layers
       geotiff_symlink=$layers_dir/templatecolored.tif
       
-      # echo "sp="$sp
-      # echo "species_dir="$species_dir
-      # echo "layers_dir="$layers_dir
-      
       rm -rf $species_dir
       mkdir $species_dir
       mkdir $layers_dir
       cp $template_dir/*.* $species_dir/
-      rm -rf $geotiff_symlink
+      rm -f $geotiff_symlink
       ln -s $geotiff_dir/$sp\.tif $geotiff_symlink
-      sed -i "s|GRSM_template|GRSM_$sp|" $project_file
-      sed -i "s|Abies_fraseri_colored.tif|$sp.tif|" $project_file
+      sed -i "s|$name_template|$dataset_prefix\_$sp|" $project_file
+      sed -i "s|$geotiff_template|$sp\.tif|" $project_file
    done
 done <<< $(ls $geotiff_dir)
--- backend/tilemillcode/export_projects.sh
+++ backend/tilemillcode/export_projects.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-if test $# -ne 2; then
-   echo 'usage: create_projects.sh mapbox_directory geotiff_directory'
+if test $# -ne 3; then
+   echo 'usage: create_projects.sh tilemill_directory mapbox_directory geotiff_directory'
    exit 1
 fi
+
+file_ext="mbtiles"
+
+tilemill_dir=${1%/}
+export_cmd='$tilemill_dir/index.js export'
+mapbox_dir=${2%/}
+export_dir=$mapbox_dir/export
+geotiff_dir=${3%/}
 
 echo $(date) > export_start_time.txt
 echo $(date +%s) > export_start_secs.txt
 
-mapbox_dir=${1%/}
-export_dir=$mapbox_dir/export
-geotiff_dir=${2%/}
-
-export PATH=$HOME/nodejslocal/bin:$PATH
-export PATH=$HOME/tilemill:$PATH
-
 while read line; do
    for sp in $line; do
       sp=${sp%.tif}
-      index.js export $sp $export_dir/$sp.mbtiles
+      $export_cmd $sp $export_dir/$sp.$file_ext
    done
 done <<< $(ls $geotiff_dir)
 
