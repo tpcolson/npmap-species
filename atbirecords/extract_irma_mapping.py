078587bb143ef1a95905ba2225d68b65edd0e973
Lonnie Yu <lonnieljyu@gmail.com>
2015-05-06 22:36:44 -0400

    Extract irma_mapping.json to ATBI_ids.txt format.

---
+++ atbirecords/extract_irma_mapping.py
@@ -0,0 +1,6 @@
+import json
+
+irma_mapping = file.read(file("irma_mapping.json"))
+species_id_dict = json.loads(irma_mapping)
+for species in sorted(species_id_dict):
+   print species +" "+ species_id_dict[species]
