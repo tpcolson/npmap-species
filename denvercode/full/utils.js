971799733f78765574c7f5ed078b830e4bd51864
John Duggan <johnduggan12@gmail.com>
2015-09-08 17:33:37 -0400

    Finish map controls layout
    Added map annotations box and moved a few pixels
    around.

--- denvercode/full/utils.js
+++ denvercode/full/utils.js
@@ -16,4 +16,26 @@ window.onload = function() {
 			NPMap.config.L.removeLayer(overlay.L);
 		}
 	}
+
+	/* move annotation tools to tophat */
+	var editControls = document.getElementsByClassName('leaflet-control-edit')[0];
+	editControls.style.boxShadow = '0px';
+	editControls.style.width = '84px';
+	editControls.style.height = '26px';
+  editControls.style.webkitBoxShadow = 'none';
+  editControls.style.mozBoxShadow = 'none';
+  editControls.style.msBoxShadow = 'none';
+  editControls.style.oBoxShadow = 'none';
+  editControls.style.boxShadow = 'none';
+	editControls.children[2].remove();
+	editControls.children[2].remove();
+	editControls.children[0].style.borderTop = '0px';
+	editControls.children[0].style.borderRadius = '4px 0px 0px 4px';
+	editControls.children[2].style.borderRadius = '0px 4px 4px 0px';
+	for(var i = 0; i < editControls.children.length; i++) {
+		editControls.children[i].style.height = '26px';
+		editControls.children[i].style.width = '26px';
+		editControls.children[i].style.float = 'left';
+	}
+	document.getElementById('options-annotations-buttons').appendChild(editControls);
 }
