407164548c2ca8ae80b794a6cb3f9b2cfc4956b4
John Duggan <johnduggan12@gmail.com>
2016-06-21 16:45:17 -0400

    add in Rafa's gdal command

--- backend/tilemillcode/upload_projects.sh
+++ backend/tilemillcode/upload_projects.sh
@@ -2,12 +2,13 @@
 
 mapbox_user="nps"
 dataset_prefix="GRSM"
-access_file=secret.txt
+access_file="secret.txt"
 access_token=$(cat $access_file)
+gdal_cmd="gdal_translate -of GTiff -a_nodata 0"
 upload_cmd="mapbox --access-token $access_token upload"
 geotiff_dir="./geotiffs"
 ids_file="./ATBI_ids.txt"
-file_ext="tif"
+ext="tif"
 
 while read line; do
 	for sp in $line; do
@@ -15,6 +16,7 @@ while read line; do
 		color=${sp##*_}
 		species_name=${sp%_*}
 		id=$(printf "%07i" $(grep -iw $species_name $ids_file | cut -d' ' -f2))
-		$upload_cmd $mapbox_user\.$dataset_prefix\_$id\_$color $geotiff_dir/$sp\_$color\.$file_ext
+		echo $gdal_cmd $geotiff_dir/$sp\.$ext $geotiff_dir/$sp\.$ext
+		echo $upload_cmd $mapbox_user\.$dataset_prefix\_$id\_$color $geotiff_dir/$sp\.$ext
 	done
 done <<< $(ls $geotiff_dir)
