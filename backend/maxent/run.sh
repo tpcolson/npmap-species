f2f4247f811a91458e49c23ec81668aa72e80bf5
3PIV <pprovins@vols.utk.ed>
2019-05-24 08:42:03 -0500

    Updated error printg on run scripts

--- backend/maxent/run.sh
+++ backend/maxent/run.sh
@@ -4,10 +4,10 @@ echo $(date) > start_time.txt
 echo $(date +%s) > start_secs.txt
 #. /usr/share/modules/init/bash
 # module load eden
-./do_run.sh 
-./preprocess.sh 
-./maxent.sh 
-./postprocess.sh
+./do_run.sh ; echo $?
+./preprocess.sh ; echo $?
+./maxent.sh ; echo $?
+./postprocess.sh ; echo $?
 #./visit.sh
 
 #eval "qsub visit.pbs -W depend=afterok:$(cat current_eden_job.txt | grep nics.utk.edu) > current_eden_job.txt" && \
