b4d24deb9ca7fa341ce73c0b51b5fc1c8e0164f6
3PIV <pprovins@vols.utk.ed>
2019-06-03 12:09:57 -0500

    Fixed gdal conversion error

--- backend/tilemillcode/upload_projects.sh
+++ backend/tilemillcode/upload_projects.sh
@@ -7,8 +7,11 @@ access_token=$(cat $access_file)
 gdal_cmd="gdal_translate -of GTiff -a_nodata 0"
 upload_cmd="mapbox --access-token $access_token upload"
 geotiff_dir="./geotiffs"
+outgeos_dir="./geotiffs/out"
 ids_file="./ATBI_ids.txt"
 ext="tif"
+
+mkdir $outgeos_dir
 
 while read line; do
 	for sp in $line; do
@@ -16,7 +19,7 @@ while read line; do
 		color=${sp##*_}
 		species_name=${sp%_*}
 		id=$(printf "%07i" $(grep -iw $species_name $ids_file | cut -d' ' -f2))
-		echo $gdal_cmd $geotiff_dir/$sp\.$ext $geotiff_dir/$sp\.$ext
-		echo $upload_cmd $mapbox_user\.$dataset_prefix\_$id\_$color $geotiff_dir/$sp\.$ext
+		echo $gdal_cmd $geotiff_dir/$sp\.$ext $outgeos_dir/$sp\.$ext
+		echo $upload_cmd $mapbox_user\.$dataset_prefix\_$id\_$color $outgeos_dir/$sp\.$ext
 	done
 done <<< $(ls $geotiff_dir)
