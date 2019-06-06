815bc6515e5d6e64d27e60dfdfa062b984b9ec7c
3PIV <pprovins@vols.utk.ed>
2019-06-06 15:39:11 -0500

    Fleshed out the dump command

--- dockerrun.sh
+++ dockerrun.sh
@@ -15,3 +15,15 @@ cd /app/npmap-species/backend/maxent/eden_maxent/ && parallel < commands
 cd /app/npmap-species/backend/maxent/eden_aggregate/ && source /app/npmap-species/backend/maxent/eden_aggregate/commands
 
 cd /app/npmap-species/backend/maxent && /app/npmap-species/backend/tilemillcode/upload_projects.sh
+
+output_dir=/app/data/output
+maxent_dir=/app/npmap-species/backend/maxent
+if [[ ! -z ${DUMP_ENV} ]]; then
+	mkdir ${output_dir}
+
+	mkdir ${output_dir}/geotiffs
+	cp ${maxent_dir}/geotiffs/out/*.tif ${output_dir}/geotiffs
+
+	mkdir ${output_dir}/maxentresults
+	cp -r ${maxent_dir}/maxent_results/* ${output_dir}/maxent_results
+fi
